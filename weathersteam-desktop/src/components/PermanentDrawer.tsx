dadeea327a67e6e22565dddd2f17fadfa5b594e1
Lautaru Vlad <lautaruvlad07@gmail.com>
2026-07-12 14:32:58 +0300

    feature: add nav buttons

--- weathersteam-desktop/src/components/PermanentDrawer.tsx
+++ weathersteam-desktop/src/components/PermanentDrawer.tsx
@@ -1,6 +1,5 @@
 import {
   Drawer,
-  Toolbar,
   Divider,
   List,
   ListItem,
@@ -8,63 +7,92 @@ import {
   ListItemIcon,
   ListItemText,
   Box,
-} from "@mui/material";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+  Typography
+} from '@mui/material';
+import VideogameAssetIcon from '@mui/icons-material/VideogameAsset';
+import SettingsIcon from '@mui/icons-material/Settings';
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 interface PermanentDrawerProps {
   drawerWidth: number;
-  appBarHeight: number;
 }
 
 export default function PermanentDrawer(props: PermanentDrawerProps) {
   return (
-    <Drawer
-      sx={{
-        width: props.drawerWidth,
-        flexShrink: 0,
-        "& .MuiDrawer-paper": {
-          display: "flex",
-          justifyContent: "space-between",
+    <Box>
+      <Drawer
+        sx={{
           width: props.drawerWidth,
-          boxSizing: "border-box",
-          top: `${props.appBarHeight}px`,
-          height: `calc(100% - ${props.appBarHeight}px)`,
-        },
-      }}
-      variant="permanent"
-      anchor="left"
-    >
-      <Box>
-        <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <ListItem key={text} disablePadding>
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            display: 'flex',
+            justifyContent: 'space-between',
+            width: props.drawerWidth,
+            boxSizing: 'border-box'
+          }
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Box>
+          <Box sx={{ display: 'flex', justifyContent: 'center', margin: 1 }}>
+            <Typography variant="h4" component="div">
+              Weather Steam
+            </Typography>
+          </Box>
+          <Divider />
+          <List>
+            <ListItem key="Profile" disablePadding>
               <ListItemButton>
                 <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  <AccountCircleIcon />
                 </ListItemIcon>
-                <ListItemText primary={text} />
+                <ListItemText primary="Profile" />
               </ListItemButton>
             </ListItem>
-          ))}
-        </List>
-        <Divider />
-      </Box>
-      <Box>
-        <Divider />
-        <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <ListItem key={text} disablePadding>
+            <ListItem key="My Library" disablePadding>
               <ListItemButton>
                 <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  <VideogameAssetIcon />
                 </ListItemIcon>
-                <ListItemText primary={text} />
+                <ListItemText primary="My Library" />
               </ListItemButton>
             </ListItem>
-          ))}
-        </List>
-      </Box>
-    </Drawer>
+            <ListItem key="Weather Choice" disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <ThunderstormIcon />
+                </ListItemIcon>
+                <ListItemText primary="Weather Choice" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem key="Random Choice" disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <QuestionMarkIcon />
+                </ListItemIcon>
+                <ListItemText primary="Random Choice" />
+              </ListItemButton>
+            </ListItem>
+          </List>
+          <Divider />
+        </Box>
+        <Box>
+          <Divider />
+          <List>
+            <ListItem key="Settings" disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <SettingsIcon />
+                </ListItemIcon>
+                <ListItemText primary="Settings" />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </Box>
+      </Drawer>
+    </Box>
   );
 }
