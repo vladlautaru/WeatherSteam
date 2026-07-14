508f6a833108a57ababda4516c06dcb3db119b43
Lautaru Vlad <lautaruvlad07@gmail.com>
2026-07-14 11:19:21 +0300

    feature: refresl location button

--- weathersteam-desktop/src/components/profile/LocalInfoContainer.tsx
+++ weathersteam-desktop/src/components/profile/LocalInfoContainer.tsx
@@ -1,6 +1,7 @@
-import { Box, CircularProgress, Typography } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { Box, Button, CircularProgress, Typography } from '@mui/material';
+import { useCallback, useEffect, useState } from 'react';
 import { LocationResponse } from '../../../common/types';
+import RefreshIcon from '@mui/icons-material/Refresh';
 
 export default function LocalInfoContainer() {
   const [loading, setLoading] = useState<boolean>(false);
@@ -8,17 +9,20 @@ export default function LocalInfoContainer() {
     status: 'fail'
   });
 
-  useEffect(() => {
-    const fetchLocation = async () => {
+  const fetchLocation = useCallback(async () => {
+    setLoading(true);
+    try {
       const currentLocationResponse =
         await window.weatherSteamApi.getCurrentLocation();
       setCurrentLocation(currentLocationResponse);
-    };
-
-    setLoading(true);
-    fetchLocation();
-    setLoading(false);
+    } finally {
+      setLoading(false);
+    }
   }, []);
+
+  useEffect(() => {
+    fetchLocation();
+  }, [fetchLocation]);
 
   const renderInfo = () => {
     if (loading) {
@@ -26,19 +30,19 @@ export default function LocalInfoContainer() {
     } else {
       return (
         <>
-          <Typography variant="h5" component="div">
+          <Typography variant="h6" component="div">
             Country:{' '}
             {currentLocation.status === 'success'
               ? currentLocation.country
               : 'unknown'}
           </Typography>
-          <Typography variant="h5" component="div">
+          <Typography variant="h6" component="div">
             Region:{' '}
             {currentLocation.status === 'success'
               ? currentLocation.regionName
               : 'unknown'}
           </Typography>
-          <Typography variant="h5" component="div">
+          <Typography variant="h6" component="div">
             Location:{' '}
             {currentLocation.status === 'success'
               ? currentLocation.city
@@ -59,6 +63,16 @@ export default function LocalInfoContainer() {
       }}
     >
       {renderInfo()}
+      <Button
+        color="secondary"
+        variant="contained"
+        startIcon={<RefreshIcon />}
+        disableRipple={true}
+        loading={loading}
+        onClick={fetchLocation}
+      >
+        Refresh
+      </Button>
     </Box>
   );
 }
